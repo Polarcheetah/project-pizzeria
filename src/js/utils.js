7b5286e8fc4ab373f0c0d953607f9175bce4d37a
Maria Berk <marysia.berk@gmail.com>
2022-01-06 11:22:15 +0100

    Add datePicker and hourPicker

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 /* global dataSource */
 
-export const utils = {}; // eslint-disable-line no-unused-vars
+const utils = {}; // eslint-disable-line no-unused-vars
 
 utils.createDOMFromHTML = function (htmlString) {
   let div = document.createElement('div');
@@ -90,3 +90,5 @@ utils.addDays = function (dateStr, days) {
   dateObj.setDate(dateObj.getDate() + days);
   return dateObj;
 };
+
+export default utils;
